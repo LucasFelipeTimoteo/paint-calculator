f3dbf581d2068219e64d20902b31a28876eacbd4
Lucas Felipe <conhecimento54321@gmail.com>
2022-01-31 19:42:33 -0300

    feature: Create doors and windows numbers calculator step

---
+++ src/parts/Calculator/CalculationFields/DoorsAndWindowsNumbers/DoorsNumber/index.tsx
@@ -0,0 +1,17 @@
+import { TextField } from '@material-ui/core'
+import React from 'react'
+import useWallsAccessoriesProvider from '../../../../../contexts/WallsAccessories/providerHooks'
+
+export default function DoorsNumber() {
+  const { doorsNumber, handleDoorsNumber } = useWallsAccessoriesProvider()
+
+  return (
+    <TextField
+      variant='filled'
+      label='Number of doors'
+      title='Type the total number of doors'
+      onChange={handleDoorsNumber}
+      value={doorsNumber}
+    />
+  )
+}
---
+++ src/parts/Calculator/CalculationFields/DoorsAndWindowsNumbers/index.tsx
@@ -0,0 +1,21 @@
+import { Slide } from '@material-ui/core'
+import React from 'react'
+import useCalculationFieldsStyles from '../styles'
+import { ISlideAnimationDirectionProp } from '../../../../hooks/useSlideAnimationDirection/types'
+import DoorsNumber from './DoorsNumber'
+import WindowsNumber from './WindowsNumber'
+
+export default function DoorsAndWindowsNumbers({
+  slideAnimationDirection
+}: ISlideAnimationDirectionProp) {
+
+  const { inputFields } = useCalculationFieldsStyles()
+  return (
+    <Slide in={true} direction={slideAnimationDirection} mountOnEnter unmountOnExit>
+      <div className={inputFields}>
+        <DoorsNumber />
+        <WindowsNumber />
+      </div>
+    </Slide>
+  )
+}
